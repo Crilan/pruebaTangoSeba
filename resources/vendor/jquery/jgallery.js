2afe8776f5a8423dbdba61ccd31543c519da8b40
Crilan <39030317+Crilan@users.noreply.github.com>
2018-10-01 19:55:20 +0100

    Add files via upload

--- resources/vendor/jquery/jgallery.js
+++ resources/vendor/jquery/jgallery.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   $('#gal1').click(function() {
-  $('#gallery1').load('galeria.html');
+  $('#target').load('galeria.html');
   //$('#gallery1').css({"display":"block"});
   $('#target').toggle('slow', function() {
     // Animation complete.

d9416b14c4a68572a46967ef08ece3abc62d9e46
Crilan <39030317+Crilan@users.noreply.github.com>
2018-10-01 15:51:36 +0100

    Update jgallery.js

--- resources/vendor/jquery/jgallery.js
+++ resources/vendor/jquery/jgallery.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
   $('#gal1').click(function() {
-    alert("hola");
+  alert("hola");
   $('#target').load('galeria.html');
   alert("hola2");
+  $('#gallery1').css({"display":"block"});
   $('#gallery1').toggle('slow', function() {
     // Animation complete.
   });
